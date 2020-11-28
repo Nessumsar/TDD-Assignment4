17ed741329aa92326281e4b433d62214d8cae98a
Nessumsar <lukasrasmusseen@gmail.com>
2020-11-28 17:17:16 +0100

    empty test methods created

---
+++ src/test/java/org/example/GameboardTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.Test;
+
+public class GameboardTest
+{
+
+    @Test
+    void testSuccessPlaceMarker(){
+
+    }
+
+    @Test
+    void testFailPlaceMarker(){
+
+    }
+
+    @Test
+    void testGetPlayedRounds(){
+
+    }
+
+    @Test
+    void testNextRoundOnWin(){
+
+    }
+
+    @Test
+    void testFinishGameOnWin(){
+
+    }
+
+    @Test
+    void testPlaceMarkerAndWin(){
+
+    }
+
+}
