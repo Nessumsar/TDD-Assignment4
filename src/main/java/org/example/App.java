b12f5110f170bc305b15fe4baa6e41e71dfb60a6
Nessumsar <lukasrasmusseen@gmail.com>
2020-11-30 12:18:09 +0100

    psuedo-code to write thoughts

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,33 @@
 package org.example;
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
+        Gameboard gameboard = new Gameboard();
+        System.out.println("Hello and welcome to 4 in a row. How many rounds would you like to play?");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        RoundCounter counter = new RoundCounter(input);
+
+        System.out.println("Lets begin.");
+        String player1 = gameboard.decideStartingPlayer();
+        String player2 = player1.equals("X") ? "O" : "X";
+
+/*      loop round
+
+        gameboard.placeMarker(player1, 1); if not ok - do again
+        if ok
+        gameboard.evaluateWin();  - if win/draw - next round
+        if not
+        gameboard.changePlayer()
+
+        endloop
+
+        gameboard.announceFinalWinner
+ */
 
     }
 }
