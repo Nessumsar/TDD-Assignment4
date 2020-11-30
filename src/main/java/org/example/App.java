d84920ce3756a6ca6e5c805a07c3d9c8839d25b8
Nessumsar <lukasrasmusseen@gmail.com>
2020-11-30 14:54:19 +0100

    main continued

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        boolean keepPlaying = true;
         System.out.println("Hello and welcome to 4 in a row. How many rounds would you like to play?");
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
@@ -14,9 +14,18 @@ public class App
         Gameboard gameboard = new Gameboard(counter);
 
         System.out.println("Lets begin.");
-        String player1 = gameboard.decideStartingPlayer();
-        String player2 = player1.equals("X") ? "O" : "X";
+        gameboard.decideStartingPlayer();
+        // player1.equals("X") ? "O" : "X";
+        System.out.println(gameboard.currentPlayer + " will start.");
 
+        for(int i=0; i<gameboard.counter.currentRound; i++){
+            while (keepPlaying){
+                System.out.println("Place your marker in a row between 1-7");
+                String row = sc.next();
+                int parsed = Integer.parseInt(row);
+
+            }
+        }
 /*      loop round
 
         gameboard.placeMarker(player1, 1); if not ok - do again
