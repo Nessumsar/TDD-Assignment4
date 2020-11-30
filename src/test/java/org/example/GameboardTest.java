b11e2f9f59d4c938734865a967f511487c025c4f
Nessumsar <lukasrasmusseen@gmail.com>
2020-11-30 13:05:16 +0100

    Assertions written

--- src/test/java/org/example/GameboardTest.java
+++ src/test/java/org/example/GameboardTest.java
@@ -1,22 +1,64 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class GameboardTest
 {
+    RoundCounter counter = null;
+    Gameboard gameboard = null;
+
+    @BeforeEach
+    void setup(){
+        counter = new RoundCounter(3);
+        gameboard = new Gameboard(counter);
+    }
+
+    @AfterEach
+    void tearDown(){
+        counter = null;
+        gameboard = null;
+    }
 
     @Test
     void testSuccessPlaceMarker(){
 
+
+        Assertions.assertEquals(true, gameboard.placeMarker("X", 1));
     }
 
     @Test
     void testFailPlaceMarker(){
 
+        gameboard.placeMarker("X", 1);
+        Assertions.assertEquals(false, gameboard.placeMarker("X", 1));
     }
 
     @Test
+    void testPlaceMarkerAndWin(){
+
+        gameboard.placeMarker("X", 1);
+        gameboard.placeMarker("X", 2);
+        gameboard.placeMarker("X", 3);
+        gameboard.placeMarker("X", 4);
+        Assertions.assertEquals("X", gameboard.evaluateWin());
+    }
+
+    @Test
+    void testFinishGameOnWin(){
+
+        Assertions.assertEquals("X", gameboard.announceFinalWinner());
+    }
+
+
+
+/*  Test here or in another test class?
+
+    @Test
     void testGetPlayedRounds(){
+
 
     }
 
@@ -25,14 +67,6 @@ public class GameboardTest
 
     }
 
-    @Test
-    void testFinishGameOnWin(){
-
-    }
-
-    @Test
-    void testPlaceMarkerAndWin(){
-
-    }
+ */
 
 }
