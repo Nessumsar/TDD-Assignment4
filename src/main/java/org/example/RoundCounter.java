e38f550895fa1a8ecc8141cd1c62e0c3a4bcc3e9
Nessumsar <lukasrasmusseen@gmail.com>
2020-12-01 12:35:11 +0100

    Score tracker

--- src/main/java/org/example/RoundCounter.java
+++ src/main/java/org/example/RoundCounter.java
@@ -2,11 +2,31 @@ package org.example;
 
 public class RoundCounter {
 
-    int currentRound = 0;
-    int maxRounds = 0;
+    private int currentRound = 0;
+    private int maxRounds = 0;
+    private int scoreO = 0;
+    private int scoreX = 0;
+
 
     public RoundCounter(int maxRounds){
         this.maxRounds = maxRounds;
+        currentRound++;
+    }
+
+    public int getScoreO() {
+        return scoreO;
+    }
+
+    public void setScoreO(int scoreO) {
+        this.scoreO = scoreO;
+    }
+
+    public int getScoreX() {
+        return scoreX;
+    }
+
+    public void setScoreX(int scoreX) {
+        this.scoreX = scoreX;
     }
 
     public int getCurrentRound() {
@@ -14,7 +34,7 @@ public class RoundCounter {
     }
 
     public boolean setCurrentRound() {
-        if (currentRound < maxRounds){
+        if (currentRound <= maxRounds){
             currentRound++;
             return true;
         }else return false;
