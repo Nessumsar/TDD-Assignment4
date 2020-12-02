6fdb2f15a0f0e401636970b452d8fbdcc508c847
Nessumsar <lukasrasmusseen@gmail.com>
2020-12-02 17:12:25 +0100

    added replaygame function

--- src/main/java/org/example/GameboardObserver.java
+++ src/main/java/org/example/GameboardObserver.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameboardObserver extends Observer {
 
@@ -10,5 +12,11 @@ public class GameboardObserver extends Observer {
     public void update(String player, String place) {
         storage.add(player + " | " + place);
         System.out.println(player + " | " + place);
+    }
+
+    @Override
+    public void replayGame() {
+        Timer timer = new Timer();
+        storage.forEach(System.out::println);
     }
 }
--- src/main/java/org/example/Observer.java
+++ src/main/java/org/example/Observer.java
@@ -2,4 +2,5 @@ package org.example;
 
 public abstract class Observer {
     public abstract void update(String player, String place);
+    public abstract void replayGame();
 }
